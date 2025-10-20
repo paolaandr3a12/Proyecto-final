123ba7917654fcd266602048f0af74c16c41fde8
paolaandr3a12 <arcilasolispaolaandrea@gmail.com>
2025-10-20 13:03:12 -0500

    elimine archivo innecesario

--- ProyectoFinal_Cursospao.java
+++
@@ -1,9 +0,0 @@
-package com.mycompany.proyectofinal_cursospao;
-
-
-public class ProyectoFinal_Cursospao {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
